261d93d291dd78425490e2a42258de5fd44073af
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2022-10-19 01:02:28 -0400

    Allowed optional pick of env var or cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use lib::*;
 pub use std::env;
 
-fn env_get() -> (String, bool) {
+fn env_get() -> (Option<String>, bool) {
     let mut args: Vec<String> = env::args().collect();
     let mut debug: bool = false;
     args.remove(0);
@@ -13,33 +13,65 @@ fn env_get() -> (String, bool) {
             debug = true;
         }
     }
-    (input, debug)
+    if input.trim().is_empty() {
+        return (None, debug);
+    }
+    (Some(input), debug)
 }
 
 fn main() {
     let (input, debug) = env_get();
-    let mut data = Data::initalize(&input).unwrap_or_else(|err| CustomError::state_error(err));
+    if let Some(input) = input {
+        let mut data = Data::initalize(&input).unwrap_or_else(|err| CustomError::state_error(err));
 
-    data.do_math()
-        .unwrap_or_else(|err| CustomError::state_error(err));
+        data.do_math()
+            .unwrap_or_else(|err| CustomError::state_error(err));
 
-    if debug {
-        println!("input: {}", input);
-        println!(
-            "num 1: {}\nop: {}\nnum 2: {:?}",
-            data.num_1, data.operator, data.num_2
-        );
-    }
+        if debug {
+            println!("input: {}", input);
+            println!(
+                "num 1: {}\nop: {}\nnum 2: {:?}",
+                data.num_1, data.operator, data.num_2
+            );
+        }
 
-    if data.num_2.is_some() {
-        println!(
-            "{} {} {} = {}",
-            data.num_1,
-            data.operator,
-            data.num_2.unwrap(),
-            data.value.unwrap()
-        );
+        if data.num_2.is_some() {
+            println!(
+                "{} {} {} = {}",
+                data.num_1,
+                data.operator,
+                data.num_2.unwrap(),
+                data.value.unwrap()
+            );
+        } else {
+            println!("{}{} = {}", data.operator, data.num_1, data.value.unwrap());
+        }
     } else {
-        println!("{}{} = {}", data.operator, data.num_1, data.value.unwrap());
+        let input = get_input().unwrap_or_else(|err| CustomError::state_error(err));
+
+        let mut data = Data::initalize(&input).unwrap_or_else(|err| CustomError::state_error(err));
+
+        data.do_math()
+            .unwrap_or_else(|err| CustomError::state_error(err));
+
+        if env::var("debug").is_ok() {
+            println!("input: {}", input);
+            println!(
+                "num 1: {}\nop: {}\nnum 2: {:?}",
+                data.num_1, data.operator, data.num_2
+            );
+        }
+
+        if data.num_2.is_some() {
+            println!(
+                "{} {} {} = {}",
+                data.num_1,
+                data.operator,
+                data.num_2.unwrap(),
+                data.value.unwrap()
+            );
+        } else {
+            println!("{}{} = {}", data.operator, data.num_1, data.value.unwrap());
+        }
     }
 }
